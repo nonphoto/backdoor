acacc28e9b1e31e15276db2b73d5d1273a8727d3
Jonas Luebbers <jluebbers@squarespace.com>
2017-12-24 18:09:04 -0500

    Clean up main

--- src/main.js
+++ src/main.js
@@ -2,46 +2,55 @@ import Vector from '@nonphoto/vector'
 
 import Grid from './grid'
 
-window.addEventListener('load', () => {
-    const canvas = document.getElementById('canvas')
+function toCanvasCoordinates(clientCoordinates) {
+
+}
+
+let mouse = new Vector()
+let grid = new Grid(12, 10, 10)
+
+let canvas = null
+let context = null
+let canvasBounds = null
+
+
+
+function load() {
+    canvas = document.getElementById('canvas')
     canvas.width = 192
     canvas.height = 128
 
-    const context = canvas.getContext('2d')
+    context = canvas.getContext('2d')
 
-    let canvasBounds = canvas.getBoundingClientRect()
-    let mouse = new Vector()
-    let grid = new Grid(12, 10, 10)
+    canvasBounds = canvas.getBoundingClientRect()
+}
 
-    function draw() {
-        context.clearRect(0, 0, canvas.width, canvas.height)
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
 
-        grid.draw(context)
+    grid.draw(context)
 
-        window.requestAnimationFrame(draw)
-    }
+    window.requestAnimationFrame(draw)
+}
 
-    function clientToCanvasCoordinates(x, y) {
-        const rx = (x - canvasBounds.left) / canvasBounds.width * canvas.width
-        const ry = (y - canvasBounds.top) / canvasBounds.height * canvas.height
 
-        return new Vector(rx, ry)
-    }
 
-    window.addEventListener('resize', () => {
-        canvasBounds = canvas.getBoundingClientRect()
-    })
-
-    document.addEventListener('mousemove', (event) => {
-        mouse = clientToCanvasCoordinates(event.clientX, event.clientY)
-    })
-
-    document.addEventListener('click', (event) => {
-        const coordinates = clientToCanvasCoordinates(event.clientX, event.clientY)
-        const index = grid.toIndex(coordinates)
-        const tile = grid.getTile(index)
-        grid.setTile(index, !tile)
-    })
-
+window.addEventListener('load', () => {
+    load()
     draw()
+})
+
+window.addEventListener('resize', () => {
+    canvasBounds = canvas.getBoundingClientRect()
+})
+
+document.addEventListener('mousemove', (event) => {
+    mouse.x = (event.clientX - canvasBounds.left) / canvasBounds.width * canvas.width
+    mouse.y = (event.clientY - canvasBounds.top) / canvasBounds.height * canvas.height
+})
+
+document.addEventListener('click', () => {
+    const index = grid.toIndex(mouse)
+    const tile = grid.getTile(index)
+    grid.setTile(index, !tile)
 })
